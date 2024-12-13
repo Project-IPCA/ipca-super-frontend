021e0cd498f0086c52e4a4a5d2fe400c368328ba
bourbxn <jakkapat97@gmail.com>
2024-12-13 20:38:45 +0700

    ✨ [APP-0029] Add filter exercise in exercise pool

--- src/features/exercisesPool/components/ExerciseCard.tsx
+++ src/features/exercisesPool/components/ExerciseCard.tsx
@@ -4,10 +4,14 @@ import {
   CardBody,
   CardFooter,
   Checkbox,
+  Option,
+  Select,
   Typography,
 } from "@material-tailwind/react";
 import { LabItem } from "../redux/ExercisesPoolSlice";
 import { useNavigate, useParams } from "react-router-dom";
+import { OPTIONS, OPTIONS_VALUE } from "../constants";
+import { useMemo, useState } from "react";
 
 interface Props {
   level: string;
@@ -28,16 +32,54 @@ function ExerciseCard({
 }: Props) {
   const navigate = useNavigate();
   const { groupId, chapterIdx } = useParams();
+  const [filter, setFilter] = useState<string>(OPTIONS_VALUE.all);
+
+  const selectedItemsSet = useMemo(
+    () => new Set(selectedItems),
+    [selectedItems],
+  );
+
   const getItemSelected = (exerciseId: string) => {
-    return !!selectedItems.find((item) => item === exerciseId);
+    return selectedItemsSet.has(exerciseId);
   };
+
+  const labItemsFiltered = useMemo(() => {
+    if (filter === OPTIONS_VALUE.selected) {
+      return labItems.filter((item) => selectedItemsSet.has(item.exercise_id));
+    } else if (filter === OPTIONS_VALUE.notSelected) {
+      return labItems.filter((item) => !selectedItemsSet.has(item.exercise_id));
+    }
+    return labItems;
+  }, [filter, labItems, selectedItemsSet]);
 
   return (
     <Card className="border-[1px] ">
       <CardBody>
-        <Typography variant="h5">Level {level}</Typography>
+        <div className="flex justify-between items-center">
+          <Typography variant="h5">Level {level}</Typography>
+          <div className="w-36">
+            <Select
+              label="Filter"
+              value={filter}
+              onChange={(val) => {
+                if (val) {
+                  setFilter(val);
+                }
+              }}
+              containerProps={{
+                className: "!min-w-28 ",
+              }}
+            >
+              {OPTIONS.map((o) => (
+                <Option key={o.value} value={o.value}>
+                  {o.label}
+                </Option>
+              ))}
+            </Select>
+          </div>
+        </div>
         <Card className="shadow-none border-[1px] mt-3 h-48 overflow-scroll">
-          {labItems.map((item) => (
+          {labItemsFiltered.map((item) => (
             <div
               className="border-b-[1px] px-8 py-1 flex justify-start gap-x-8 items-center"
               key={item.exercise_id}
@@ -58,6 +100,11 @@ function ExerciseCard({
               </Typography>
             </div>
           ))}
+          {labItemsFiltered.length === 0 && (
+            <div className="w-full h-full flex justify-center items-center">
+              <Typography>No Exercise Available</Typography>
+            </div>
+          )}
         </Card>
       </CardBody>
       <CardFooter className="flex justify-end pt-0">
