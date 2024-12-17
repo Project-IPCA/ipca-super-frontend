64a4a733729907a62236652d948c774f2e2c594d
bourbxn <jakkapat97@gmail.com>
2024-12-17 16:35:35 +0700

    ✨ [APP-0034] Edit spinner loading UI

--- src/components/spinnerLoading/SpinnerLoading.tsx
+++ src/components/spinnerLoading/SpinnerLoading.tsx
@@ -2,8 +2,8 @@ import { Spinner } from "@material-tailwind/react";
 
 const SpinnerLoading = () => {
   return (
-    <div>
-      <Spinner className="h-8 w-8" />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Spinner className="h-12 w-12" />
     </div>
   );
 };
